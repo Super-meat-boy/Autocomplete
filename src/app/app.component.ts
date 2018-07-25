dcaed11ddc831b299c4afa88b2250449c2a031f4
fabien terrasse <fabien.terrasse.pf1@gmail.com>
2018-07-25 15:35:59 +0200

    test autocomplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {CreateNewAutocompleteGroup, NgAutocompleteComponent, SelectedAutocompleteItem} from 'ng-auto-complete';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild(NgAutocompleteComponent) public completer: NgAutocompleteComponent;
+
+  public group = [];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.group = [CreateNewAutocompleteGroup(
+      'Please Select a Flow from the List',
+      'completer',
+      [
+        {title: 'Flux ARTXXXXXCX', id: '1', frequency: 'test'},
+        {title: 'Flux CIGOGNEXXXXXXC', id: '2', frequency: 'test'},
+        {title: 'CFTCGFDBVFHDTGFTGT', id: '3', frequency: 'test'},
+        {title: 'FTPXXXX', id: '4', frequency: 'test'},
+        {title: 'FTPXZZZ', id: '5', frequency: 'toto'},
+        {title: 'FTPXRTG', id: '6', frequency: 'tata'},
+        {title: 'FTPEEEE', id: '7', frequency: 'hello'},
+        {title: 'FTPDDDD', id: '8', frequency: 'warning'},
+        {title: 'IDFEGRGTERZGETREZFS', id: '9'},
+      ],
+      {titleKey: 'title', childrenKey: null,}),]
+    ;
+  }
+
+  /**
+   *
+   * @param item
+   * @constructor
+   */
+  Selected(item: SelectedAutocompleteItem) {
+    console.log(item);
+  }
 }
